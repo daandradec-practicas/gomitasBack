1f0f568b5940ee165aab4f02da7506ea5339dbf5
Duvan Alberto Andrade Cuenca <daandradec@unal.edu.co>
2019-05-08 00:35:21 +0000

    library api

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,48 @@
 class BooksController < ApplicationController
+    
 
+    # GET
     def index
+        id = params[:author_id]
+        author = Author.find(id)
+        books = author.books
+        render json:books, status:200
     end
-
-    def show
-    end
-
+    
+    # POST
     def create
+        author = Author.new(params_author)
+        if author.save
+            render json: author, status:201
+        else
+            render json: author.errors, status: :unprocessable_entity
+        end
     end
-
+    
+    # GET
+    def show
+        id = params[:id]
+        author = Author.find(id)
+        render json: author, status:200
+    end
+    
+    # PUT, PATCH
     def update
+        id = params[:id]
+        author = Author.find(id)
+        if author.update(params_author)
+            render json: author, status: :updated_entity
+        else
+            render json: author.errors, status: :unprocessable_entity
+        end
     end
-
-    def delete
+    
+    # DELETE
+    def destroy
+    end
+    
+    def params_author
+        params.require(:author).permit(:name, :age)
     end
 
 
