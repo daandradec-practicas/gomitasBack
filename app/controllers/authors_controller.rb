4614072d5aaf3b5f39c1b00921c73b8a66382713
Duvan Alberto Andrade Cuenca <daandradec@unal.edu.co>
2019-05-08 00:15:00 +0000

    authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,17 +1,48 @@
 class AuthorsController < ApplicationController
+    # GET
     def index
-        render json: Author.all
+        #render json: Author.all
+        authors = Author.all
+        #respond_to? do |format|
+        #    format.xml {render xml: authors, status: 200}
+        #    format.json {render json: authors, status: 200}
+        #end
+        render json:authors, status:200
     end
+    
+    # POST
     def create
         author = Author.new(params_author)
         if author.save
             render json: author, status:201
         else
-            render json: author.errors, status: unprocessable_entity
+            render json: author.errors, status: :unprocessable_entity
         end
     end
     
+    # GET
+    def show
+        id = params[:id]
+        author = Author.find(id)
+        render json: author, status:200
+    end
+    
+    # PUT, PATCH
+    def update
+        id = params[:id]
+        author = Author.find(id)
+        if author.update(params_author)
+            render json: author, status: :updated_entity
+        else
+            render json: author.errors, status: :unprocessable_entity
+        end
+    end
+    
+    # DELETE
+    def destroy
+    end
+    
     def params_author
-        params.require(:author).permit(:name)
+        params.require(:author).permit(:name, :age)
     end
 end
